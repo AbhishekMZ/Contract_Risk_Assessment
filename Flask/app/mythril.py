a22160c696052c7f379e082aec1a28f77def4600
sahana <spmundewadi@ucdavis.edu>
2018-05-15 16:54:02 -0700

    IT WORKS

--- Flask/app/mythril.py
+++ Flask/app/mythril.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a862b12f9d0dded14626861aff3a220e011363587e991b66d097dd301a8a7dd
-size 2107
+oid sha256:3a5e93bbd421ee6525113dbf2b9799883f0bd9a9f4991b405b42fdefcfaddccc
+size 2178
