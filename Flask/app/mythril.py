38d574d1cacf90fc110d9bc5d9c55b659ecaf297
sahana <spmundewadi@ucdavis.edu>
2018-05-15 17:27:37 -0700

    minor fix

--- Flask/app/mythril.py
+++ Flask/app/mythril.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:837e674f1ddae13e07d19a189295468bb48720ad712a38c4dad879343c1d55ac
-size 2387
+oid sha256:6a0d7c8cbb16a58dfbc27341e914fee44e0aa70b3b72d492d702cb03ad7ca2ff
+size 2385
