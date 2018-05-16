6a9f2f76246a353a074f30c8e6e8ea0e91b42821
sahana <spmundewadi@ucdavis.edu>
2018-05-15 17:26:31 -0700

    added line numbers in json output

--- Flask/app/mythril.py
+++ Flask/app/mythril.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a5e93bbd421ee6525113dbf2b9799883f0bd9a9f4991b405b42fdefcfaddccc
-size 2178
+oid sha256:837e674f1ddae13e07d19a189295468bb48720ad712a38c4dad879343c1d55ac
+size 2387
