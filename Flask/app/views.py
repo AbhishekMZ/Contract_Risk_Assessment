6027b5207b1b10030ffea92c8bfddef3d9f7ecd9
Doug Sherman <dsherman@ucdavis.edu>
2018-05-17 21:36:19 -0700

    Ran mythril and oyente on all scripts including mutations

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fc9a00692ea0b2bf27f612f3bcf629552ca022c895e88045ac0f740716f3c5a
-size 3244
+oid sha256:a572149534deeaeb1c2f4e6da7b4ad9c6d084ee7b273e86d2826115ca4e63ae2
+size 3337
---
+++ TestRuns/build_graphs.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5a916f331791eaa642ee66fc10cd36c49406c7a2d9996df832e65ea64cd263c
+size 5312
