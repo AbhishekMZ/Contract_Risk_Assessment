695e4864b371d71abde07d4ce151164476a6ba7c
Vincent Yang <vincenty.376@gmail.com>
2018-05-29 16:20:02 -0700

    Update smartcheck

---
+++ Flask/app/smartcheck.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:727290f206d64c08ed49fbfdfb9274a68be9532f2fb3fe275b4db4d0579428ae
+size 2069
--- Flask/app/views.py
+++ Flask/app/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9f571b15bc037612257432d648de1773b87507ea89d99134ea5c55eb00a41fb
-size 5062
+oid sha256:21bd781803aba3d53b85e0e8e9b262df7dcf4e7e84caaf1c0c206e34255906f6
+size 5462
