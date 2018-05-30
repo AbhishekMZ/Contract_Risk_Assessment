def17471ce173c911987737aa185422b3758d036
Vincent Yang <vincenty.376@gmail.com>
2018-05-29 22:39:15 -0700

    Fixed bug on skipping first error in Oyente

--- Flask/app/oyente.py
+++ Flask/app/oyente.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91e01e3e9d555b4a5f3919fcd534d2dac02fcae168dba360d3d67f3ad8ab947
-size 4561
+oid sha256:8c1b070321dab6ef3217d35bf885818afd39cea071f31bf6181fb108d6741e22
+size 4690
