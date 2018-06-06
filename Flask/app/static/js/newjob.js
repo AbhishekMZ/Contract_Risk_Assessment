a51be15fdc8dba371cad6298ca5358b000c087d3
jkotlarek <jtkotlarek@gmail.com>
2018-06-06 16:52:37 -0700

    added Line Tagging (saves selection in global array taggedRows[] in newjob.js)

--- Flask/app/static/js/newjob.js
+++ Flask/app/static/js/newjob.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52a7b3bc263019cae09121e5b30ad1537917b5fe5bb5add99005b0bc62affb24
-size 10937
+oid sha256:4d7829d9cd5595405f12897370ce50f94c6faa204f04142f682874cfe559eecf
+size 11598
