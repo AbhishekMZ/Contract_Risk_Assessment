92994dc863fb1ef858c938c7c4b91ff6d8473028
AbhishekMZ <mabhishekz.ai22@rvce.edu.in>
2025-05-26 20:34:54 +0530

    Data update

---
+++ External_Contracts/test_contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.24;
+
+contract Reentrancy {
+    mapping(address => uint) private userBalances;
+
+    function deposit() public payable {
+        userBalances[msg.sender] += msg.value;
+    }
+
+    function withdrawBalance() public {
+        uint amountToWithdraw = userBalances[msg.sender];
+        // This is vulnerable to reentrancy
+        if (msg.sender.call.value(amountToWithdraw)()) {
+            userBalances[msg.sender] = 0;
+        }
+    }
+}
